9a234bae45125f33ceb48296c7082779ad1cd6a0
Matthew Rankin <matthew@cumulusware.com>
2017-06-13 09:41:55 -0500

    First refactor to handle more than USB VISA strings

---
+++ constants.go
@@ -0,0 +1,35 @@
+package visa
+
+type interfaceType uint16
+type viVersion uint32
+type viRsrc string
+type viAccessMode uint32
+
+const (
+	noLock        viAccessMode = 0 // VI_NO_LOCK
+	exclusiveLock viAccessMode = 1 // VI_EXCLUSIVE_LOCK
+	sharedLock    viAccessMode = 2 // VI_SHARED_LOCK
+	loadConfig    viAccessMode = 4 // VI_LOAD_CONFIG
+)
+
+// Hardware interface types given as VI_ATTR_INTF_TYPE in the VISA standard.
+const (
+	gpibInterface    interfaceType = 1
+	vxiInterface     interfaceType = 2
+	gpibVxiInterface interfaceType = 3
+	asrlInterface    interfaceType = 4
+	pxiInterface     interfaceType = 5
+	tcpipInterface   interfaceType = 6
+	usbInterface     interfaceType = 7
+)
+
+type resourceClass string
+
+const (
+	instrResource     resourceClass = "INSTR"
+	memaccResource    resourceClass = "MEMACC"
+	intfcResource     resourceClass = "INTFC"
+	backplaneResource resourceClass = "BACKPLANE"
+	servantResource   resourceClass = "SERVANT"
+	socketResource    resourceClass = "SOCKET"
+)
--- visa.go
+++ visa.go
@@ -2,86 +2,138 @@ package visa
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-// Resource represents the resource for a VISA enabled piece of test equipment.
-type Resource struct {
-	ResourceString string
-	InterfaceType  string
-	BoardIndex     uint16
-	ManufacturerID uint16
-	ModelCode      uint16
-	SerialNumber   string
-	InterfaceIndex uint16
-	ResourceClass  string
+// Resource defines the interface for VISA Resources.
+type Resource interface {
+	GetResourceClass() string
+}
+
+// The ResourceTemplate struct fields come from Table 3.2.1 VISA Template
+// Required Attributes in the VPP-4.3 The VISA Library standard.
+type ResourceTemplate struct {
+	ResourceImplementedVersion viVersion     // VI_ATTR_RSRC_IMPL_VERSION
+	ResourceLockState          viAccessMode  // VI_ATTR_RSRC_LOCK_STATE
+	ResourceManufacturerID     uint          // VI_ATTR_RSRC_MANF_ID
+	ResourceManufacturerName   string        // VI_ATTR_RSRC_MANF_NAME
+	ResourceName               viRsrc        // VI_ATTR_RSRC_NAME
+	ResourceSpecVersion        viVersion     // VI_ATTR_RSRC_SPEC_VERSION
+	resourceClass              resourceClass // VI_ATTR_RSRC_CLASS
+}
+
+func (resource ResourceTemplate) GetResourceClass() string {
+	return string(resource.resourceClass)
+}
+
+type genericInstrResource struct {
+	InterfaceNum            int           // VI_ATTR_INTF_NUM
+	InterfaceType           interfaceType // VI_ATTR_INTF_TYPE
+	InterfaceInstrumentName string        // VI_ATTR_INTF_INST_NAME
+}
+
+// UsbInstrResource represents a VISA Instrument Control Resource using the USB
+// interface.
+type UsbInstrResource struct {
+	ResourceTemplate
+	genericInstrResource
+	CompliantWith4882 bool   // VI_ATTR_4882_COMPLIANT
+	ManufacturerID    uint16 // VI_ATTR_MANF_ID
+	ModelCode         uint16 // VI_ATTR_MODEL_CODE
+	ManufacturerName  string // VI_ATTR_MANF_NAME
+	ModelName         string // VI_ATTR_MODEL_NAME
+	SerialNumber      string // VI_ATTR_USB_SERIAL_NUM
+	UsbInterfaceNum   uint16 // VI_ATTR_USB_INTFC_NUM
+	MaxInterruptSize  uint16 // VI_ATTR_USB_MAX_INTR_SIZE
+	UsbProtocol       int16  // VI_ATTR_USB_PROTOCOL
+}
+
+// AsrlInstrResource represents a VISA Instrument Control Resource using an
+// asynchronous serial (ASRL) hardware interface.
+type AsrlInstrResource struct {
+	Resource
+	Port uint16
 }
 
 // NewResource creates a new Resource using the given VISA resourceString.
-func NewResource(resourceString string) (visa *Resource, err error) {
-	visa = &Resource{
-		ResourceString: resourceString,
-		InterfaceType:  "",
-		BoardIndex:     0,
-		ManufacturerID: 0,
-		ModelCode:      0,
-		SerialNumber:   "",
-		InterfaceIndex: 0,
-		ResourceClass:  "",
-	}
-	regString := `^(?P<interfaceType>[A-Za-z]+)(?P<boardIndex>\d*)::` +
-		`(?P<manufacturerID>[^\s:]+)::` +
-		`(?P<modelCode>[^\s:]+)` +
-		`(::(?P<serialNumber>[^\s:]+))?` +
-		`::(?P<resourceClass>[^\s:]+)$`
-
-	re := regexp.MustCompile(regString)
+func NewResource(resourceString string) (Resource, error) {
+	regexString := `^(?P<interfaceType>[A-Za-z]+)(?P<boardIndex>\d*)::` +
+		`(?P<allElse>.*)$`
+	re := regexp.MustCompile(regexString)
 	res := re.FindStringSubmatch(resourceString)
 	subexpNames := re.SubexpNames()
 	matchMap := map[string]string{}
 	for i, n := range res {
 		matchMap[subexpNames[i]] = string(n)
 	}
-
-	// TODO(mdr): Need to accept more than just USB interface types.
-	if strings.ToUpper(matchMap["interfaceType"]) != "USB" {
-		return visa, errors.New("visa: interface type was not usb")
+	interfaceType := strings.ToUpper(matchMap["interfaceType"])
+	// Determine the boardIndex if one was given, if not default to 0
+	boardIndex := 0
+	i, err := strconv.Atoi(matchMap["boardIndex"])
+	if err != nil && matchMap["boardIndex"] != "" {
+		return nil, fmt.Errorf("Bad board index parsing resource string: %s", err)
+	} else if err == nil {
+		boardIndex = i
 	}
-	visa.InterfaceType = "USB"
+	allElse := strings.ToUpper(matchMap["allElse"])
 
-	if matchMap["boardIndex"] != "" {
-		boardIndex, err := strconv.ParseUint(matchMap["boardIndex"], 0, 16)
-		if err != nil {
-			return visa, errors.New("visa: boardIndex error")
-		}
-		visa.BoardIndex = uint16(boardIndex)
+	if interfaceType == "USB" {
+		return createUSBResource(boardIndex, allElse)
+	} else if interfaceType == "TCPIP" {
+		return createTCPIPResource(resourceString)
+	} else {
+		return nil, errors.New("Interface type not supported")
 	}
+
+}
+
+func createTCPIPResource(resourceString string) (Resource, error) {
+	return nil, errors.New("createTCPIPResource hasn't been implemented yet")
+}
+
+func createUSBResource(boardIndex int, partialResource string) (Resource, error) {
+	regexString := `(?P<manufacturerID>[^\s:]+)::` +
+		`(?P<modelCode>[^\s:]+)` +
+		`(::(?P<serialNumber>[^\s:]+))?` +
+		`::(?P<resourceClass>[^\s:]+)$`
+	re := regexp.MustCompile(regexString)
+	res := re.FindStringSubmatch(partialResource)
+	subexpNames := re.SubexpNames()
+	matchMap := map[string]string{}
+	for i, n := range res {
+		matchMap[subexpNames[i]] = string(n)
+	}
+	if matchMap["resourceClass"] != "INSTR" {
+		return nil, errors.New("visa: resource class was not instr")
+	}
+
+	var resource UsbInstrResource
+
+	resource.InterfaceNum = boardIndex
+	resource.InterfaceType = usbInterface
+	resource.resourceClass = instrResource
 
 	if matchMap["manufacturerID"] != "" {
 		manufacturerID, err := strconv.ParseUint(matchMap["manufacturerID"], 0, 16)
 		if err != nil {
-			return visa, errors.New("visa: manufacturerID error")
+			return resource, errors.New("visa: manufacturerID error")
 		}
-		visa.ManufacturerID = uint16(manufacturerID)
+		resource.ManufacturerID = uint16(manufacturerID)
 	}
 
 	if matchMap["modelCode"] != "" {
 		modelCode, err := strconv.ParseUint(matchMap["modelCode"], 0, 16)
 		if err != nil {
-			return visa, errors.New("visa: modelCode error")
+			return resource, errors.New("visa: modelCode error")
 		}
-		visa.ModelCode = uint16(modelCode)
+		resource.ModelCode = uint16(modelCode)
 	}
 
-	visa.SerialNumber = matchMap["serialNumber"]
+	// visa.SerialNumber = matchMap["serialNumber"]
 
-	if strings.ToUpper(matchMap["resourceClass"]) != "INSTR" {
-		return visa, errors.New("visa: resource class was not instr")
-	}
-	visa.ResourceClass = "INSTR"
-
-	return visa, nil
+	return resource, nil
 
 }
