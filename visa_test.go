0bdc0add47b4a65edd5834a9ac40ce35c715d05e
Matthew Rankin <matthew@cumulusware.com>
2017-06-23 21:47:44 -0500

    Add copyright info to files.

--- visa_test.go
+++ visa_test.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 The visa developers. All rights reserved.
+// Project site: https://github.com/gotmc/ivi
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
 package visa
 
 import (
