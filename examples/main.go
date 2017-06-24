922f6ac08d4334a9a9278f3d9e6d9dd4012eb299
Matthew Rankin <matthew@cumulusware.com>
2017-06-23 22:07:26 -0500

    Fix copyright and project links

--- examples/main.go
+++ examples/main.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2017 The ivi developers. All rights reserved.
-// Project site: https://github.com/gotmc/ivi
+// Copyright (c) 2017 The visa developers. All rights reserved.
+// Project site: https://github.com/gotmc/visa
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
 
--- helpers.go
+++ helpers.go
@@ -1,4 +1,5 @@
-// Project site: https://github.com/gotmc/ivi
+// Copyright (c) 2017 The visa developers. All rights reserved.
+// Project site: https://github.com/gotmc/visa
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
 
